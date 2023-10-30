bd9c9280fe6ea5567e32e8d9ad67af55f5e2bff3
Malek Ghozzi <malekghozzi97@gmail.com>
2023-10-30 22:09:23 +0100

    add search sction

--- RazorMovieApp/Pages/Movies/Index.cshtml.cs
+++ RazorMovieApp/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorMovieApp.Data;
 using RazorMovieApp.Models;
@@ -20,13 +21,29 @@ namespace RazorMovieApp.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }  
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public string? MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            if (_context.Movie != null)
+            //Use LINQ
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+
+            var movies = from m in _context.Movie select m;
+            if (!string.IsNullOrEmpty(SearchString))
             {
-                Movie = await _context.Movie.ToListAsync();
+                movies = movies.Where(s => s.Title.Contains(SearchString));
             }
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
